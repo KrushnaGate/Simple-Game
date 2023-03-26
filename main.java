53750b9e6db6ca5f8810e395b34e5b82e70a9e61
KrushnaGate <122196314+KrushnaGate@users.noreply.github.com>
2023-03-26 18:39:26 +0530

    .
    .

---
+++ main.java
@@ -0,0 +1,15 @@
+package com.krushna;
+
+public class main {
+    public static  void main(String args[]){
+
+        player1 player=new player1( "Krushna","Sord",100);
+
+        player.damageByGun1();
+        player.damageByGun1();
+        player.damageBYGun2();
+        player.heal();
+       // player2 betterplayer=new player2("sam","machine gun",80,false);
+        //betterplayer.damageByGun1();
+    }
+}
